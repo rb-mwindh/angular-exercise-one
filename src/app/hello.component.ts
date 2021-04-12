b839d78903af98a0dab7c4ea7e75c3c92d5924f3
rb-mwindh <markus.windhager@de.bosch.com>
2021-04-12 15:02:46 +0200

    2 files modified, 1 added

---
+++ src/app/hello.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "hello",
+  template: '<h1 [class.red]="isRed">Hello World!</h1>',
+  styles: ["h1 { color: black; }", "h1.red { color: red; }"]
+})
+export class HelloComponent {
+  @Input() isRed = false;
+}
