7e72cea2c20af688e8362dc0f0be623ce1470cd6
rb-mwindh <markus.windhager@de.bosch.com>
2021-04-29 09:44:14 +0200

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,12 +2,14 @@ import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "hello",
-  template: '<h1 [class.red]="_isRed">Hello World!</h1>',
+  template: '<h1 [class.red]="isRed">Hello World!</h1>',
   styles: ["h1 { color: black; }", "h1.red { color: red; }"]
 })
 export class HelloComponent {
   isRed = false;
-  @Input('isRed') set _isRed(arg: any) {
-    this.isRed = !!arg && arg !== "false";
+  @Input("isRed") set _isRed(arg: any) {
+    console.log(arg);
+    this.isRed = !arg || arg === "true";
+    console.log("this.isRed: ", this.isRed);
   }
 }
